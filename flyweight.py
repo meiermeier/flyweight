2369cc6c9135ad317c317185820f4609bf3400fd
meiermeier <57707823+meiermeier@users.noreply.github.com>
2021-11-10 19:27:08 +0100

    Update flyweight.py
    Replaced _registry name to _pool and type from dict to weakref.WeakValueDictionary

--- flyweight.py
+++ flyweight.py
@@ -1,27 +1,15 @@
+import weakref
+
 class Flyweight(type):
     def __init__(cls, *args, **kwargs):
-        cls._registry = {}
+        cls._pool = weakref.WeakValueDictionary()
         super().__init__(*args, **kwargs)
 
     def __call__(cls, *args, **kwargs):
         data = (args, tuple(kwargs.items()))
 
-        if not (instance := cls._registry.get(data)):
-            instance = cls._registry[data] = super().__call__(*args, **kwargs)
-
+        instance = cls._pool.get(data)
+        if not instance:
+            instance = cls._pool[data] = super().__call__(*args, **kwargs)
+            
         return instance
-
-
-if __name__ == '__main__':
-    class A(metaclass=Flyweight):
-
-        def __init__(self, a):
-            print('init', a)
-
-
-    A(1)
-    A(1)
-    A(1)
-    A(2)
-    A(2)
-    A(2)
